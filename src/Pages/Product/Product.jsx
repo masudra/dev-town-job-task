4c1d1b3f29db9a0771a8f923210c58b4cfc17d5e
masudra <masudrana6278312@gmail.com>
2023-11-02 13:02:51 +0600

    sort add

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,10 +1,9 @@
 const Product = ({ singelData }) => {
-    const { image, title, category, price, description } = singelData
+    const { image, title, category, price, description, _id } = singelData
+    console.log(singelData);
+    
     return (
         <div >
-
-        
-
             <div className="card  bg-base-100 shadow-xl h-[400px]">
                 <figure><img className="p-5" src={image} alt="" /></figure>
                 <div className="card-body">
@@ -14,7 +13,7 @@ const Product = ({ singelData }) => {
                     <button className="btn" onClick={() => document.getElementById('my_modal_2').showModal()}>More Details</button>
                     <dialog id="my_modal_2" className="modal">
                         <div className="modal-box">
-                            <figure><img className="w-[200px]" src={image} alt="" /></figure>
+                            <figure><img className="w-[200px] hover:w-[400px]" src={image} alt="" /></figure>
                             <h3 className="font-bold text-lg">{title}</h3>
                             <p className="py-4">${price}</p>
                             <p className="py-4">{description}</p>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -3,27 +3,30 @@ import Product from "../Product/Product";
 
 const Products = () => {
     const [data, setData] = useState([])
+    const [asc, setAsc] = useState(true)
     useEffect(() => {
-        const url = ('../../../public/data.json')
-        fetch(url)
+        fetch(`http://localhost:5000/phones?sort=${asc?'asc': 'deasc'}`)
             .then(res => res.json())
             .then(data => setData(data))
-    }, [])
+    }, [asc])
+
+
     return (
         <div>
-            <div className="flex  gap-2 ml-10">
-                <h1 className="my-auto">Sort By:</h1>
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn m-1">Click</label>
-                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
-                    </ul>
+            <div className="flex  gap-2 justify-end mr-20 text-xl my-4">
+                <h1 className="my-auto font-bold">Sort By:</h1>
+                <div>
+                    <select onClick={() => (setAsc(!asc))} >
+                        {
+                            asc ? <option>Low to High Price</option> :
+                                <option>High to Low Price</option>
+                        }
+                    </select>
                 </div>
             </div>
             <div className="grid grid-cols-4 gap-5 mx-5">
                 {
-                    data.map(singelData => <Product singelData={singelData}></Product>)
+                    data.map(singelData => <Product key={singelData?._id} singelData={singelData}></Product>)
                 }
             </div>
         </div>
